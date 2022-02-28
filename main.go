2898582e61334192d4954d6e363bd180ea5e59b8
Rajesh Pandhare <rajesh.pandhare@kanakasoftware.com>
2022-02-28 23:58:34 +0530

    basic check-in

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
 )
+//Defind data model
+type Book struck{
+	ID		string		`json:"id"`
+}
 
 func main(){
-fmt.Println ("Hey this is mt first go program which i ran")
+//Init Router 
+router := mux.NewRouter()
+
+//Handler Function/End Points
+router.HandleFunc("/api/books",getBooks).Methods("GET")
+router.HandleFunc("/api/books/{id}",getBook).Methods("GET")
+router.HandleFunc("/api/books",createBooks).Methods("POST")
+router.HandleFunc("/api/books/{id}",updateBooks).Methods("PUT")
+router.HandleFunc("/api/books/{id}",deleteBooks).Methods("DELETE")
+
+//Listen on port 
+log.Fatal(http.ListenAndServe(":8000",router))
+
+
+
 }
